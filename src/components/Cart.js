e1a3c7c504982d88f3b71fc8e9151bc3d0a11dc4
Francisco Payes <payesfrancisco@gmail.com>
2018-07-18 18:54:07 -0400

    feat: move backward and forward in form

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
 import { getOrderName } from '../helpers/order';
-import { TYPES } from '../conf/order';
 import AddressForm  from './AddressForm';
 
 class Cart extends  Component {
@@ -8,28 +8,55 @@ class Cart extends  Component {
     super(props);
 
     this.state = {
-      step: props.type === TYPES.DELIVERY ? 'order-address' : 'choose-quantity'
+      step: 0
+    };
+
+    this.goNextStep = this.goNextStep.bind(this);
+    this.goPreviousStep = this.goPreviousStep.bind(this);
+  }
+
+  goPreviousStep() {
+    const { step } = this.state;
+    const previousStep = step - 1;
+
+    if (previousStep < 0) {
+      this.props.history.push('/');
+    } else {
+      this.setState({
+        step: step - 1
+      });
     }
   }
+
+  goNextStep() {
+    this.setState({
+      step: this.state.step + 1
+    })
+  }
+
   render () {
     const { step } = this.state;
+    const backButtonText = step === 0 ? 'Volver a empezar' : 'Regresar';
 
     return (
       <div className="container">
         <div className="card shadow-lg">
-          <h2 className="card-header">
-            {getOrderName(this.props.type)}
-          </h2>
+          <div className="card-header">
+            <h2>{getOrderName(this.props.type)}</h2>
+          </div>
           <div className="card-body">
             <div className="row">
               <div className="col-12">
-                {step === 'order-address' &&
-                <AddressForm />
+                {step === 0 &&
+                  <AddressForm />
+                }
+                {step === 1 &&
+                  <h2>Number of pizzas</h2>
                 }
               </div>
               <div className="col-12 px-4 pt-5 pb-3">
-                <button className="btn btn-danger float-left">Regresar</button>
-                <button className="btn btn-success float-right">Siguiente</button>
+                <button className="btn btn-danger float-left" onClick={this.goPreviousStep}>{backButtonText}</button>
+                <button className="btn btn-success float-right" onClick={this.goNextStep}>Siguiente</button>
               </div>
             </div>
           </div>
@@ -39,4 +66,4 @@ class Cart extends  Component {
   }
 };
 
-export default Cart;
+export default withRouter(Cart);
