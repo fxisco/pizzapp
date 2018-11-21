6a3656c11963f050b811ecf09538147a4ab5c756
Francisco Payes <payesfrancisco@gmail.com>
2018-11-20 22:05:00 -0400

    Edit product

--- src/components/Products.js
+++ src/components/Products.js
@@ -12,13 +12,19 @@ class Products extends Component {
     this.state = {
       name: '',
       price: '',
-      products: {}
+      selectedId: '',
+      products: {},
+      showAddForm: false,
+      showEditForm: false
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.addProduct = this.addProduct.bind(this);
+    this.handleEditProduct = this.handleEditProduct.bind(this);
+    this.saveProduct = this.saveProduct.bind(this);
     this.resetForm = this.resetForm.bind(this);
     this.subscribeListener = this.subscribeListener.bind(this);
+    this.showForm = this.showForm.bind(this);
+    this.handleProductActivation = this.handleProductActivation.bind(this);
   }
 
   componentDidMount () {
@@ -31,16 +37,27 @@ class Products extends Component {
     });
   }
 
-  addProduct () {
+  saveProduct () {
     const newRegistry = this.productsRef.doc();
-    const { name, price = 0 } = this.state;
-
-    this.productsRef.doc(newRegistry.id).set({
-      id: newRegistry.id,
-      active: true,
+    const {
       name,
-      price: parseInt(price)
-    });
+      price = 0,
+      showEditForm,
+      selectedId
+    } = this.state;
+
+    if (showEditForm) {
+      this.productsRef
+        .doc(selectedId)
+        .set({ price }, { merge: true })
+    } else {
+      this.productsRef.doc(newRegistry.id).set({
+        id: newRegistry.id,
+        active: true,
+        name,
+        price: parseInt(price)
+      });
+    }
 
     this.resetForm();
   }
@@ -48,7 +65,9 @@ class Products extends Component {
   resetForm () {
     this.setState({
       name: '',
-      price: ''
+      price: '',
+      showAddForm: false,
+      showEditForm: false
     });
   }
 
@@ -87,10 +106,35 @@ class Products extends Component {
       });
   }
 
+  showForm () {
+    this.setState({
+      showAddForm: true
+    })
+  }
 
+  handleEditProduct(id) {
+    const { products } = this.state;
+
+    this.setState({
+      name: products[id].name,
+      price: products[id].price,
+      showEditForm: true,
+      selectedId: id
+    })
+  }
+
+  handleProductActivation(id) {
+    const {
+      products
+    } = this.state;
+
+    this.productsRef
+      .doc(id)
+      .set({ active: !products[id].active }, { merge: true });
+  }
 
   render() {
-    const { name, price, products } = this.state;
+    const { name, price, products, showAddForm, showEditForm } = this.state;
 
     return (
       <div className="container">
@@ -100,30 +144,39 @@ class Products extends Component {
           </div>
         </div>
         <div className="row">
+          <div className="col text-left">
+            {(!showAddForm && !showEditForm) &&
+              <button className="btn btn-success" onClick={this.showForm}>Agregar Producto</button>
+            }
+          </div>
+        </div>
+        <div className="row">
           <div className="col-sm-12">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title text-left">Nuevo producto</h5>
-                <div className="row">
-                  <div className="col-sm-6">
-                    <div className="form-group">
-                      <label htmlFor="productName">Nombre</label>
-                      <input name="name" className="form-control" type="text" id="productName" value={name} onChange={this.handleInputChange} />
+            {(showAddForm || showEditForm) &&
+              <div className="card product-form">
+                <div className="card-body">
+                  <h5 className="card-title text-left">{showEditForm ? 'Editar Producto' : 'Nuevo Producto'}</h5>
+                    <div className="row">
+                      <div className="col-sm-6">
+                        <div className="form-group">
+                          <label htmlFor="productName">Nombre</label>
+                          <input name="name" className="form-control" type="text" id="productName" value={name} onChange={this.handleInputChange} readOnly={showEditForm} />
+                        </div>
+                      </div>
+                      <div className="col-sm-6">
+                        <div className="form-group">
+                          <label htmlFor="productPrice">Precio</label>
+                          <input name="price" className="form-control" type="text" id="productPrice" onChange={this.handleInputChange} onKeyPress={handleOnlyNumbersValidation} value={price} />
+                        </div>
+                      </div>
+                      <div className="col-12 text-right">
+                        <button className="btn btn-danger mr-2" onClick={this.resetForm}>Cancelar</button>
+                        {name && price && <button className="btn btn-success" onClick={this.saveProduct}>Guardar</button>}
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-sm-6">
-                    <div className="form-group">
-                      <label htmlFor="productPrice">Precio</label>
-                      <input name="price" className="form-control" type="text" id="productPrice" onChange={this.handleInputChange} onKeyPress={handleOnlyNumbersValidation} value={price} />
-                    </div>
-                  </div>
-                  <div className="col-12 text-right">
-                    <button className="btn btn-danger mr-2" onClick={this.resetForm}>Cancelar</button>
-                    {name && price && <button className="btn btn-success" onClick={this.addProduct}>Guardar</button>}
-                  </div>
                 </div>
               </div>
-            </div>
+            }
           </div>
         </div>
         <div className="row my-5">
@@ -132,9 +185,20 @@ class Products extends Component {
 
             return (
               <div key={`product-${key}`} className="col-sm-12 col-md-6 col-lg-3">
-                <div className="card">
+                <div className="card product-card">
                   <div className="card-body">
-                    <div className="text-right"><button type="button" className={`btn btn-sm ${item.active ? 'btn-success' : 'btn-danger' }`}>{item.active ? 'Activo': 'Desactivado'}</button></div>
+                    <div className="actions">
+                      <button
+                        type="button"
+                        className={`btn btn-sm ${item.active ? 'btn-success' : 'btn-danger' }`}
+                        onClick={this.handleProductActivation.bind(null, key)}
+                      >
+                        {item.active ? 'Activo': 'Desactivado'}
+                      </button>
+                      <button className="action-button" onClick={this.handleEditProduct.bind(null, key)}>
+                        <i className="fa fa-pencil px-1" />
+                      </button>
+                    </div>
                     <h5 className="card-title text-left">{item.name}</h5>
                     <div><b>Price</b>: ${item.price}</div>
                   </div>
